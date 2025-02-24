a215a3a8e567e253791fc7b84cca7800ebcaad50
boshyz <72839898+boshyz@users.noreply.github.com>
2025-02-24 21:09:49 +1100

    Update with docstrings and notes bosh_f1_latest_update.py

--- bosh_f1_latest_update.py
+++ bosh_f1_latest_update.py
@@ -1,4 +1,4 @@
-# bakc dates the races for the seasons 
+# back dates the lastest seasons, rounds for race & sprint results plus lap data 
 import requests
 import pandas as pd
 import numpy as np
@@ -12,25 +12,52 @@ import openpyxl
 
 from bosh_f1_race_results import db_races_update
 from bosh_f1_sprint_results import db_sprint_update
-from bosh_f1_season_schedule import get_season_schedule , db_seasons_update
+from bosh_f1_season_schedule import db_seasons_update
 from bosh_f1_get_laps_data import get_laps_data
-from utils import get_engine, latest_laps_update_excel,backdate_laps_data_excel
+from utils import get_engine, latest_laps_update_excel,latest_points_update_excel 
 #set up engine enter the database name in as str change accordingly
 #use.env to set up your own URI crendentials
 engine = get_engine('bosh_f1')
 
+
+"""BACKDATING POINTS AND LAPS DATA FOR LAST 30 DAYS
+
+to back points data for race and sprints if applicable for round
+exports to taleau files to populate tableau
+then updates laps data for round
+all data is updated filtered for deleted and retrieved if it is within the 30 days of today's date
+
+
+ """
+
+
+#CODE STARTS HERE change args!!!
+
+
+schema = 'f1_dash'
+season_table = 'season'
+race_table = 'race'
+sprint_table = 'sprint'
+laps_table = 'lap'
+col = 'season'
+year = 2025
+
+
+
+
+# only run this if the latest schedule is not in database uncomment below line 38
+#db_seasons_update(engine,schema, season_table, year)
+
 #update race data to database
-#db_races_update(engine, 'f1_dash', 'race')
-#update laps data to excel for tableau
-#last_30_day_update_excel(engine, 'f1_dash', 'race')
-#update laps data to database
-#get_laps_data(engine, 'f1_dash', 'lap')
-#update laps data to excel for tableau
-#last_30_day_update_excel(engine, 'f1_dash', 'lap')
+db_races_update(engine, schema, race_table)
 #update sprint data
-#db_sprint_update(engine, 'f1_dash', 'sprint')
-#update sprint data to excel for tableau
-# update scea scheudle
-#db_seasons_update(engine,'f1_dash', 'season',2025)
-#get_laps_data(engine, 'f1_dash', 'lap', 2002  )
-#backdate_laps_data_excel(engine,'f1_dash','season','lap')
+db_sprint_update(engine, schema, sprint_table)
+#update latest points data to excel for tableau
+latest_points_update_excel(engine,schema,col,race_table, sprint_table)
+#update laps data to database
+get_laps_data(engine,  schema, laps_table)
+#update laps data to excel for tableau
+latest_laps_update_excel(engine,schema,col,laps_table)
+
+
+
